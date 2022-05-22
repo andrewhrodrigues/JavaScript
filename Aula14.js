f47cf7e09088fbd0073f637023a72d9f24fbdb10
Andrew Rodrigues <105614099+andrewhrodrigues@users.noreply.github.com>
2022-05-22 19:22:22 -0300

    Funções

---
+++ Aula14.js
@@ -0,0 +1,14 @@
+//Função
+
+
+//1) Declara a função
+
+function imprimeTexto(texto){
+console.log(texto)
+
+}
+
+//2) Executa a função (1 ou mais vezes)
+
+imprimeTexto("Oi");
+imprimeTexto("Outro texto");
