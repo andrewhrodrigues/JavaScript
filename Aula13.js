1b99ba23babfb089b5aa26faafcc367e1b3e98cd
Andrew Rodrigues <105614099+andrewhrodrigues@users.noreply.github.com>
2022-05-22 19:05:01 -0300

    Template strings

---
+++ Aula13.js
@@ -0,0 +1,14 @@
+//Template strings
+
+const nome = "Andrew Rodrigues";
+const idade = 2022-1989;
+const cidadeDeNascimento = "Macapá";
+
+
+//const apresentacao = "meu nome é " + nome + " minha idade é " + idade + " e nasci na cidade de " + cidadeDeNascimento;
+
+const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`;
+
+
+
+console.log(apresentacao);
