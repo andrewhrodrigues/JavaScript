54ae950e98645c6d57cd74f3d85a75876f5cba57
Andrew Rodrigues <105614099+andrewhrodrigues@users.noreply.github.com>
2022-05-22 17:25:11 -0300

    boolean

---
+++ Aula07.js
@@ -0,0 +1,14 @@
+//boolean 
+
+const usuarioLogado = true;
+const contapaga = false;
+
+//thuthy ou falsy
+//0 => false`
+//1 => true
+
+console.log(0 == false);
+console.log("" == false);
+console.log(1 == true);
+
+
