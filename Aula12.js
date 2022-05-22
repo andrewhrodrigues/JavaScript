1b99491a04393b0ef4fa5da1514f4502304c22ff
Andrew Rodrigues <105614099+andrewhrodrigues@users.noreply.github.com>
2022-05-22 18:45:49 -0300

    Operador ternário

---
+++ Aula12.js
@@ -0,0 +1,17 @@
+2//Operador ternário
+
+const idadeMinima = 18;
+const idadeCliente = 16;
+
+
+/*if(idadeCliente >= idadeMinima){
+console.log("cerveja")
+}else{
+console.log("suco")
+
+}
+*/
+
+console.log(idadeCliente >= idadeMinima? "cerveja": "suco")
+;
+
